033b02a30e85173d4db21fecfe319a5c7cca6f48
Austin Asoluka <stracool9@gmail.com>
2023-07-04 21:47:28 +0100

    generate action icons

--- src/components/organisms/ToolBar/index.jsx
+++ src/components/organisms/ToolBar/index.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { Icon } from "../../atoms";
 
 export const ToolBar = () => {
-  return <div className="toolbar">Toolbar</div>;
+  return (
+    <div className="toolbar">
+      <div>08:45PM | DYEN Extra classes</div>
+      <div className="meeting-actions">
+        <div className="action-icon">
+          <Icon name="audio-on" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="video-off" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="caption" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="emoji" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="share-screen" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="raise-hand" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="more-options-vertical" fill="white" />
+        </div>
+        <div className="action-icon">
+          <Icon name="phone" fill="white" />
+        </div>
+      </div>
+      <div className="display-icons">Display</div>
+    </div>
+  );
 };
